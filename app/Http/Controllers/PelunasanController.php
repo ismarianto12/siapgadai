d1f00f2784a55e6698724dffca55bb96ecaea6dd
Ismarianto Syafril <ismarianto.syafril@mncgroup.com>
2024-02-02 14:36:38 +0700

    asdad

--- app/Http/Controllers/PelunasanController.php
+++ app/Http/Controllers/PelunasanController.php
@@ -35,9 +35,7 @@ class PelunasanController extends Controller
                 'akses menu ini hanya bisa diakases oleh operator / kasir.',
             ]);
             die;
-        } else {
-            return true;
-        }
+        }  
         $title = 'Pelunasan';
         $data = [];
         return view($this->view . "index", compact("title", "data"));
